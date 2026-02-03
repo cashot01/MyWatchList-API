00b491e7ab472e2601ef0511299e5bc02695c7c8
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2026-02-03 11:03:40 -0300

    initial commit

---
+++ src/main/java/com/project/mywatchlist/MyWatchListApplication.java
@@ -0,0 +1,13 @@
+package com.project.mywatchlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWatchListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWatchListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/mywatchlist/MyWatchListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.mywatchlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWatchListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
